dddef4a4cb9a473bdadee7c3fa90f4b183b69095
Irlan Freitas <irlan.freitas@hotmail.com>
2018-07-03 17:03:19 -0300

    Ajuste de tipo de sessão para o AutorBean

--- jsf-jpa-livraria/src/br/com/caelum/livraria/bean/AutorBean.java
+++ jsf-jpa-livraria/src/br/com/caelum/livraria/bean/AutorBean.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.utils.RedirectView;
 
 @ManagedBean
+@ViewScoped
 public class AutorBean {
 
 	private Autor autor = new Autor();
